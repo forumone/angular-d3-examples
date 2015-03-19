30fc71eb70c8501779c5048020b1797ed1d6cbc9
William Hurley <whurley@forumone.com>
2015-03-19 11:15:48 -0400

    Fixing

--- src/js/components/stockTicker/index.js
+++ src/js/components/stockTicker/index.js
@@ -4,82 +4,87 @@ angular.module('d3examples').directive('stockTicker', ['$interval', function($in
     restrict : 'E',
     scope : {},
     link : function(scope, element, attrs) {
-      var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+      var margin = {
+          top: 20,
+          right: 20,
+          bottom: 30,
+          left: 50
+      };
+
+      var width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
 
       var parseDate = d3.time.format("%d-%b-%y").parse;
-    
+
       var x = d3.time.scale()
+          .domain([new Date(), new Date()])
           .range([0, width]);
-    
+
       var y = d3.scale.linear()
-        .domain([0, 100])
-        .range([height, 0]);
-    
-      var xAxis = d3.svg.axis()
+          .domain([0, 100])
+          .range([height, 0]);
+
+      scope.xAxis = d3.svg.axis()
           .scale(x)
           .orient("bottom");
-    
-      var yAxis = d3.svg.axis()
+
+      scope.yAxis = d3.svg.axis()
           .scale(y)
           .orient("left");
-    
-      var line = d3.svg.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.close); });
-    
-      var svg = d3.select(element[0]).append("svg")
+
+      scope.line = d3.svg.line()
+          .x(function (d) { return x(d.date); })
+          .y(function (d) { return y(d.close); });
+
+      scope.svg = d3.select(element[0])
+          .append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+          .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+
       scope.data = [];
-      
-      $interval(function() {
-        var newVal = {
-          date : new Date(),
-          close : Math.floor(Math.random() * 100)
-        };
-        
-        scope.data.push(newVal);
 
-      }, 1000);
-      
-      svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+      scope.xSel = scope.svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(scope.xAxis);
 
-      svg.append("g")
+      scope.ySel = scope.svg.append("g")
           .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
+          .call(scope.yAxis)
+          .append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 6)
           .attr("dy", ".71em")
           .style("text-anchor", "end")
           .text("Price ($)");
-    
-      svg.append("path")
-        .datum(scope.data)
-        .attr("class", "line")
-        .attr("d", line);
-  
+
+      scope.path = scope.svg.append("path")
+          .datum(scope.data)
+          .attr("class", "line")
+          .attr("d", scope.line);
+      
+      var interval = $interval(function() {
+        scope.data.push({
+          date: new Date(),
+          close: Math.random() * 100
+        });
+      }, 1000);
+      
       scope.$watchCollection('data', function() {
-        y.domain(d3.extent(scope.data, function(d) {
-          return d.date;
-        }));
+        x.domain(d3.extent(scope.data, function (d) { return +d.date; }));
         
-        svg.selectAll(".line")
-        .data(scope.data)
-        .enter()
-          .append("path")
-          .attr("class","line")
-          .attr("d", line);
+        scope.path.datum(scope.data)
+          .transition()
+          .attr('d', scope.line);
+  
+        scope.xSel.transition().call(scope.xAxis);
         
-      });
+        if (100 < scope.data.length) {
+          $interval.cancel(interval);
+        }
+      }); 
     }
   }
 }]);
